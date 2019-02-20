c152d598664f82ae146cb83246341537d8be23f4
arshad408 <arshadachu408@gmail.com>
2019-02-20 13:50:49 +0530

    Create README.py

---
+++ README.py
@@ -0,0 +1,40 @@
+
+def g(a,b):
+	k=0
+	o=''
+	p=0
+	q=0
+	for i in range(len(a)):
+		if len(a)<len(b):
+			if a[i]==b[i]:
+				o+=a[i]
+				k=k+1
+		else :
+			q=1
+			break
+	i=0
+	if q==1:
+		while(True):
+			if a[i]==b[i]:
+				o+=a[i]
+			else :
+				o+=b[i]
+				p=p+1
+			if i==3:
+				break
+			i=i+1
+		return p
+
+	for i in range(k,len(b)):
+		o+=b[i]
+		p=p+1
+	return p
+ 
+ def main():
+	a=input()
+	b=input()
+	print(g(a,b))
+try:
+ 	 main()
+expept:
+ 	 print('invalid'
